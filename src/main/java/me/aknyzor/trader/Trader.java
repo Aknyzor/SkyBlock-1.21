d1f68122b0773415e6a89e323bdfdc470a39956f
Aknyzor <danijelzivkovic2001@gmail.com>
2024-06-27 18:11:48 +0200

    Izmenjena funkcija npca

--- src/main/java/me/aknyzor/trader/Trader.java
+++ src/main/java/me/aknyzor/trader/Trader.java
@@ -1,13 +1,6 @@
 package me.aknyzor.trader;
 
-import net.citizensnpcs.api.CitizensAPI;
-import net.citizensnpcs.api.npc.NPC;
-import net.citizensnpcs.api.npc.NPCRegistry;
-import net.citizensnpcs.trait.CommandTrait;
-import net.citizensnpcs.trait.LookClose;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
@@ -15,16 +8,16 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
+/**
+ *
+ * I dalje je u izradi (Aknyzor)
+ * 27.6.2024 Napomena: Dodati da proverava da li je npc spawnovan zbog bolje optimizacije, i odraditi malo bolje kod.
+ *
+ */
+
 public class Trader {
-
-    /**
-     *
-     * I dalje je u izradi (Aknyzor)
-     *
-     */
-
     private final JavaPlugin plugin;
-    private NPC npc;
+    private final String npcName = "Trgovac";
     private final List<String> cities = Arrays.asList("Grad1", "Grad2", "Grad3");
     private final List<String> villages = Arrays.asList("Selo1", "Selo2", "Selo3");
     private final Random random = new Random();
@@ -34,70 +27,45 @@ public class Trader {
     }
 
     public void scheduleNPC() {
-        Bukkit.getScheduler().runTaskTimer(plugin, this::checkAndCreateNPC, 0L, 1200L);
+        Bukkit.getScheduler().runTaskTimer(plugin, this::checkAndCreateNPC, 0L, 20L);
     }
 
     private void checkAndCreateNPC() {
         Calendar calendar = Calendar.getInstance();
-        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
 
-        if ((dayOfWeek == Calendar.TUESDAY || dayOfWeek == Calendar.FRIDAY) && hourOfDay == 1) {
-            if (npc == null || !npc.isSpawned()) {
-                createNPC();
-            }
-        } else if ((dayOfWeek == Calendar.TUESDAY || dayOfWeek == Calendar.FRIDAY) && hourOfDay == 23) {
-            if (npc != null && npc.isSpawned()) {
-                npc.destroy();
-                npc = null;
-            }
+        if ((day == Calendar.THURSDAY || day == Calendar.FRIDAY)
+                && hour == 18 && minute == 10 && second == 0) {
+            createNPC();
+        }else if ((day == Calendar.THURSDAY || day == Calendar.FRIDAY)
+                && hour == 23 && minute == 0 && second == 0) {
+            destroyNPC();
         }
     }
 
     private void createNPC() {
-        NPCRegistry registry = CitizensAPI.getNPCRegistry();
-        npc = registry.createNPC(EntityType.PLAYER, "Trgovac");
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "npc create " + npcName + " --type player --at 11,63,0,world");
 
-        Location location = new Location(Bukkit.getWorld("world"), 0, 64, 0);
-        npc.spawn(location);
-        npc.getOrAddTrait(LookClose.class).lookClose(true);
+        String destination;
+        destination = (random.nextBoolean() ? cities : villages).get(random.nextInt(3));
 
-        String destination = (random.nextBoolean() ? cities : villages).get(random.nextInt(3));
         plugin.getLogger().info("NPC ide u " + destination);
 
-        CommandTrait commandTrait = npc.getOrAddTrait(CommandTrait.class);
-
-        switch (destination) {
-            case "Grad1" -> {
-                CommandTrait.NPCCommandBuilder commandBuilder = new CommandTrait.NPCCommandBuilder("grad1", CommandTrait.Hand.RIGHT);
-                commandTrait.addCommand(commandBuilder);
-            }
-            case "Grad2" -> {
-                CommandTrait.NPCCommandBuilder commandBuilder = new CommandTrait.NPCCommandBuilder("grad2", CommandTrait.Hand.RIGHT);
-                commandTrait.addCommand(commandBuilder);
-            }
-            case "Grad3" -> {
-                CommandTrait.NPCCommandBuilder commandBuilder = new CommandTrait.NPCCommandBuilder("grad3", CommandTrait.Hand.RIGHT);
-                commandTrait.addCommand(commandBuilder);
-            }
-            case "Selo1" -> {
-                CommandTrait.NPCCommandBuilder commandBuilder = new CommandTrait.NPCCommandBuilder("selo1", CommandTrait.Hand.RIGHT);
-                commandTrait.addCommand(commandBuilder);
-            }
-            case "Selo2" -> {
-                CommandTrait.NPCCommandBuilder commandBuilder = new CommandTrait.NPCCommandBuilder("selo2", CommandTrait.Hand.RIGHT);
-                commandTrait.addCommand(commandBuilder);
-            }
-            case "Selo3" -> {
-                CommandTrait.NPCCommandBuilder commandBuilder = new CommandTrait.NPCCommandBuilder("selo3", CommandTrait.Hand.RIGHT);
-                commandTrait.addCommand(commandBuilder);
-            }
+        if (destination != null) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "npc select " + npcName);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "npc command add --hand right " + destination);
         }
     }
 
+    private void destroyNPC() {
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "npc select " + npcName);
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "npc remove");
+    }
+
     public void onDisable() {
-        if (npc != null && npc.isSpawned()) {
-            npc.destroy();
-        }
+        destroyNPC();
     }
 }
